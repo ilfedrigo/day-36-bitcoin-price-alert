a4d4816b9ef43b8bff44aa8a8de29a855f688984
Lucas Fedrigo <contato.fedrigo@gmail.com>
2024-02-08 14:45:10 -0300

    Get last two prices, compare and show headlines if greater than 5%

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import requests
 STOCK = "BTC"
 COMPANY_NAME = "Bitcoin"
 
-## STEP 1: Use https://www.alphavantage.co
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+# Stocks infos
 stocks_url = "https://www.alphavantage.co/query"
 stocks_api_key = "JY2F55IRE1GQJUU0"
 
@@ -14,50 +13,33 @@ stocks_params = {
     "market": "USD",
     "apikey": stocks_api_key
 }
-
-response = requests.get(stocks_url, params=stocks_params)
-data = response.json()["Time Series (Digital Currency Daily)"]
-data_list = [value for (key, value) in data.items()]
-yesterday_data = data_list[0]
-day_before_yesterday = data_list[1]
-
-day_before_yesterday_price = float(day_before_yesterday["4b. close (USD)"])
-last_day_price = float(yesterday_data["4b. close (USD)"])
-
-difference = abs(last_day_price - day_before_yesterday_price)
-diff_percent = (difference / float(last_day_price)) * 100
-
-print(diff_percent)
-
-# print(f"{day_before_yesterday_price:.2f}")
-# print(f"{last_day_price:.2f}")
-
-
-## STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+# News infos
 news_url = "https://newsapi.org/v2/everything"
 news_api_key = "154abd91a5f14f76b225a3bb2b45167a"
 
 news_params = {
+    "apiKey": news_api_key,
     "q": "Bitcoin",
     "from": "2024-02-07",
     "sortBy": "popularity", 
-    "apiKey": news_api_key,
 }
 
+response = requests.get(stocks_url, params=stocks_params)
+data = response.json()["Time Series (Digital Currency Daily)"]
+data_list = [value for (key, value) in data.items()]
+yesterday_data = data_list[1]
+day_before_yesterday_data = data_list[2]
 
-## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number. 
+yesterday_price = float(yesterday_data["4b. close (USD)"])
+day_before_yesterday_price = float(day_before_yesterday_data["4b. close (USD)"])
 
+difference = abs(yesterday_price - day_before_yesterday_price)
+diff_percent = (difference / float(yesterday_price)) * 100
 
-#Optional: Format the SMS message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
+if abs(diff_percent) > 5:
+    news_response = requests.get(news_url, params=news_params)
+    articles = news_response.json()["articles"]
+    three_articles = articles[:3]
+    formated_articles = [f"Headline: {article['title']}." for article in three_articles]
 
+    print(formated_articles)
